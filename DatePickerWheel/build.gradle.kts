a561d3952a730ff52dc226580e3b64831132354f
Vasu <badboy.exe@outlook.com>
2025-06-19 23:14:24 +0530

    commit

--- DatePickerWheel/build.gradle.kts
+++ DatePickerWheel/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
 
                 groupId = "com.github.VASU-ARDESHANA"
                 artifactId = "DatePickerWheel"
-                version = "1.0.0"
+                version = "0.0.1"
 
                 pom {
                     name.set("DatePickerWheel")
