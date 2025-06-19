d36a0f7518ada5986d14a875e3d28d4ddde4db40
Vasu <badboy.exe@outlook.com>
2025-06-20 00:13:37 +0530

    change the layout

--- DatePickerWheel/build.gradle.kts
+++ DatePickerWheel/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
 
                 groupId = "com.github.VASU-ARDESHANA"
                 artifactId = "DatePickerWheel"
-                version = "0.0.1"
+                version = "0.0.2"
 
                 pom {
                     name.set("DatePickerWheel")
